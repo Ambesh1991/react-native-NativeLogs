6ab9773e8173e25930b4798398c0baef6af81a2e
Aeris\ambesh.kukreja <ambesh.kukreja@aeris.net>
2017-09-25 13:05:42 +0530

    commiting the code after implementing logs at JS layer for Debug mode

--- index.js
+++ index.js
@@ -3,28 +3,41 @@ import { NativeModules } from 'react-native';
 
 const { RNReactNativeNativeLogs } = NativeModules;
 
-export const LOG_LEVEL={
-    INFO:'info',
-    ERROR:'error',
-    VERBOSE:'verbose',
-    WARN:'warn'
+export const LOG_LEVEL = {
+    INFO: 'info',
+    ERROR: 'error',
+    VERBOSE: 'verbose',
+    WARN: 'warn'
 }
-const NativeLogs={};
+const NativeLogs = {};
 
-NativeLogs.log=(logLevel,logTag,logMessage)=>{
-    switch(logLevel){
+NativeLogs.log = (logLevel, logTag, logMessage) => {
+    if (__DEV__) {
+        if (logLevel === LOG_LEVEL.INFO) {
+            console.info(logTag, logMessage);
+        } else if (logLevel === LOG_LEVEL.WARN) {
+            console.warn(logTag, logMessage);
+        }
+       else if (logLevel === LOG_LEVEL.ERROR) {
+            console.error(logTag, logMessage);
+        }
+        else{
+             console.info(logTag, logMessage);
+        }
+    }
+    switch (logLevel) {
         case LOG_LEVEL.INFO:
-        RNReactNativeNativeLogs.logInfo(logTag,JSON.stringify(logMessage));
-        break;
+            RNReactNativeNativeLogs.logInfo(logTag, JSON.stringify(logMessage));
+            break;
         case LOG_LEVEL.ERROR:
-        RNReactNativeNativeLogs.logError(logTag,JSON.stringify(logMessage));
-        break;
+            RNReactNativeNativeLogs.logError(logTag, JSON.stringify(logMessage));
+            break;
         case LOG_LEVEL.WARN:
-        RNReactNativeNativeLogs.logWarn(logTag,JSON.stringify(logMessage));
-        break;
+            RNReactNativeNativeLogs.logWarn(logTag, JSON.stringify(logMessage));
+            break;
         case LOG_LEVEL.VERBOSE:
-        RNReactNativeNativeLogs.logVerbose(logTag,JSON.stringify(logMessage));
-        break;
+            RNReactNativeNativeLogs.logVerbose(logTag, JSON.stringify(logMessage));
+            break;
 
     }
 }
